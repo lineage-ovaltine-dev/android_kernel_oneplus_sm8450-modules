6aa42599091180b83dfd0f1e1fba9678edcdb22f
Weikai Kong <priv@pppig236.com>
2025-05-06 13:41:27 -0400

    audio-kernel: Import oplus changes

--- qcom/opensource/audio-kernel/asoc/codecs/wcd938x/wcd938x.c
+++ qcom/opensource/audio-kernel/asoc/codecs/wcd938x/wcd938x.c
@@ -42,7 +42,13 @@
 #define ADC_MODE_VAL_ULP1     0x09
 #define ADC_MODE_VAL_ULP2     0x0B
 
+
+#ifdef OPLUS_ARCH_EXTENDS
+/* Solve the problem that get swr-dev fails after adsp ssr */
+#define NUM_ATTEMPTS 50
+#else /* OPLUS_ARCH_EXTENDS */
 #define NUM_ATTEMPTS 20
+#endif /* OPLUS_ARCH_EXTENDS */
 
 #define DAPM_MICBIAS1_STANDALONE "MIC BIAS1 Standalone"
 #define DAPM_MICBIAS2_STANDALONE "MIC BIAS2 Standalone"
@@ -2164,6 +2170,13 @@ static int wcd938x_get_logical_addr(struct swr_device *swr_dev)
 		/* retry after 4ms */
 		usleep_range(4000, 4010);
 		ret = swr_get_logical_dev_num(swr_dev, swr_dev->addr, &devnum);
+		#ifdef OPLUS_ARCH_EXTENDS
+		/*add log to check retry num issue*/
+		if (ret) {
+			dev_err(&swr_dev->dev, "%s get devnum %d for dev addr %llx failed, ret = %d, num_retry = %d",
+			__func__, devnum, swr_dev->addr, ret, num_retry);
+		}
+		#endif /*OPLUS_ARCH_EXTENDS*/
 	} while (ret && --num_retry);
 
 	if (ret)
@@ -2260,6 +2273,10 @@ static int wcd938x_event_notify(struct notifier_block *block,
 						     NULL);
 		wcd938x->mbhc->wcd_mbhc.deinit_in_progress = true;
 		mbhc = &wcd938x->mbhc->wcd_mbhc;
+		#ifdef OPLUS_ARCH_EXTENDS
+		mbhc->plug_before_ssr = mbhc->current_plug;
+		pr_info("%s: mbhc->plug_before_ssr=%d\n", __func__, mbhc->plug_before_ssr);
+		#endif /* OPLUS_ARCH_EXTENDS */
 		wcd938x->usbc_hs_status = get_usbc_hs_status(component,
 						mbhc->mbhc_cfg);
 		wcd938x_mbhc_ssr_down(wcd938x->mbhc, component);
