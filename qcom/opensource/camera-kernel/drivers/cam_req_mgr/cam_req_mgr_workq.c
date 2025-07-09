5dcf2119166f0bd1027bec8d35883514eef13e01
Weikai Kong <priv@pppig236.com>
2025-07-09 12:01:44 -0400

    cam_req_mgr: Remove duplicated sched struct
    not needed after https://lore.kernel.org/r/20230808030357.1213829-1-kolyshkin@gmail.com
    
    Change-Id: I6adbc019c45c9b6edca2aaa0b8a0abacb494dad1
    Signed-off-by: Weikai Kong <priv@pppig236.com>

--- qcom/opensource/camera-kernel/drivers/cam_req_mgr/cam_req_mgr_workq.c
+++ qcom/opensource/camera-kernel/drivers/cam_req_mgr/cam_req_mgr_workq.c
@@ -9,10 +9,6 @@
 #include "cam_common_util.h"
 #include <linux/sched/types.h>
 
-struct sched_param {
-	int sched_priority;
-};
-
 #define WORKQ_ACQUIRE_LOCK(workq, flags) {\
 	if ((workq)->in_irq) \
 		spin_lock_irqsave(&(workq)->lock_bh, (flags)); \
